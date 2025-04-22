c942fd4d442ca4cafbf5f626ee176acb711458e6
Rodrigo <lucianorodrigocarvalho@gmail.com>
2025-04-22 15:41:07 -0300

    DTO's Role and User created

---
+++ src/main/java/com/devsuperior/bds04/dto/RoleDTO.java
@@ -0,0 +1,41 @@
+
+
+package com.devsuperior.bds04.dto;
+
+import com.devsuperior.bds04.entities.Role;
+
+public class RoleDTO {
+
+	private long id;
+	private String authority;
+	
+	public RoleDTO() {
+		
+	}
+
+	public RoleDTO(long id, String authority) {
+		this.id = id;
+		this.authority = authority;
+	}
+
+	public RoleDTO(Role role) {
+		id = role.getId();
+		authority = role.getAuthority();
+	}
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+	
+}
